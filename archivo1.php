0236332088dea87d37fb48859183e23c0fa155f6
Daniel Otero <daniielotero.3136568419@gmail.com>
2017-11-20 11:02:24 -0500

    Commit init

--- archivo1.php
+++ archivo1.php
@@ -1,1 +1,11 @@
-s
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>GitHub</title>
+</head>
+<body>
+	<h1>Mi primer proyecto en GitHub</h1>
+
+</body>
+</html>
---
+++ h1.php
@@ -0,0 +1,16 @@
+<html>
+
+	<head>
+		<title></title>
+	</head>
+
+	<body>
+		<h1>Conectado GIT con GITHUB</h1>
+		<p>estos son mis cambios de GIT</p>
+
+	</body>
+
+
+
+
+</html>
